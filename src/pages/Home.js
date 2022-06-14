678563efeca111abe57b82d6a5bb6601cee49566
Renee Jiang <reneejiang@Renees-MBP.fios-router.home>
2022-06-13 22:13:08 -0400

    fixed blank page bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import AddTodo from "../component/AddTodo.js";
+import Todos from "../component/todos.js";
+import "../pages/Home.css";
   
 class Home extends Component {
   // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
-      // create your empty list here call it todos.
+      todos:[]
     };
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
@@ -16,9 +19,12 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
+    <AddTodo addTodo={this.addTodo()} />
+
     todo.id = Math.random();
     // An array that contains the current array and the new todo item
     let new_list = [...this.state.todos, todo];
+
     // Updates the local state with the new array.
     this.setState({
       todos: new_list,
@@ -28,7 +34,7 @@ class Home extends Component {
     return (
       <div className="Home">
         <h1>Todo's </h1>
-        <p> Replace this</p>
+        <Todos todos= {this.state.todos} />
       </div>
     );
   }
